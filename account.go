d329c579d741c9eb9edb955592ce72227f4967d9
Mykel Dee <kdankyira.development@gmail.com>
2022-03-05 14:08:43 +0000

    initial commit

--- account.go
+++ account.go
@@ -1,22 +1,27 @@
 package main
 
+//Import requisite libraries
 import (
 	"fmt"
 	"log"
 	"os"
 )
 
-type accountHolder struct {
-	accountName string
-	userClass   string
-	balance     float64
-}
-
+//declare constant values for userclass
 const (
 	Default = "Default"
 	VIP     = "VIP"
 )
 
+//declare accountHolder structure
+type accountHolder struct {
+	accountName string
+	userClass   string
+	balance     float64
+	PIN         int
+}
+
+//function to make new accountHolder types
 func newAccount(name string) accountHolder {
 	account := accountHolder{
 		accountName: name,
@@ -26,27 +31,36 @@ func newAccount(name string) accountHolder {
 	return account
 }
 
+//receiver function on accountholder to debit balance
 func (acc *accountHolder) debit(amt float64) {
 	acc.balance -= amt
 	fmt.Println(amt, "successfully debited from your account")
 	fmt.Println("Your new balance is ", acc.balance)
 }
 
+//receiver function on accountholder to credit balance
 func (acc *accountHolder) credit(amt float64) {
 	acc.balance += amt
 	fmt.Println(amt, "successfully credited to your account")
 	fmt.Println("Your new balance is ", acc.balance)
 }
 
+//receiver function on accountHolder information
 func (acc *accountHolder) format() string {
-	fs := "\n--------Account Details-------- \n"
-	fs += fmt.Sprintf("%-20v ... %v \n", "Name:", acc.accountName)
-	fs += fmt.Sprintf("%-20v ... %v \n", "Account Type:", acc.userClass)
-	fs += fmt.Sprintf("%-20v ... ₦%.2f \n", "Account Balance:", acc.balance)
-
+	fs := ""
+	switch acc.accountName {
+	case "":
+		fs = "\nNo Account to Format\n"
+	default:
+		fs = "\n--------Account Details-------- \n"
+		fs += fmt.Sprintf("%-20v ... %v \n", "Name:", acc.accountName)
+		fs += fmt.Sprintf("%-20v ... %v \n", "Account Type:", acc.userClass)
+		fs += fmt.Sprintf("%-20v ... ₦%.2f \n", "Account Balance:", acc.balance)
+	}
 	return fs
 }
 
+//receiver function on accountholder to save accountHolder information to txt file
 func (acc *accountHolder) save() {
 	data := []byte(acc.format())
 
--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+// import requisite libraries
 import (
 	"bufio"
 	"fmt"
@@ -8,8 +9,7 @@ import (
 	"strings"
 )
 
-const PIN uint64 = 1235
-
+//function to get input from console
 func getInput(prompt string, r *bufio.Reader) (string, error) {
 	fmt.Print(prompt)
 	input, err := r.ReadString('\n')
@@ -18,20 +18,11 @@ func getInput(prompt string, r *bufio.Reader) (string, error) {
 
 }
 
-func createAccountHolder() accountHolder {
+//function to create new accountHolders
+func createAccountHolder(user map[int64]string) accountHolder {
 	reader := bufio.NewReader(os.Stdin)
-
-	userName, _ := getInput("Create a new Account:", reader)
-
-	acc := newAccount(userName)
-	fmt.Println("User created - ", acc.accountName)
-
-	return acc
-}
-
-func accountOptions(acc *accountHolder) {
-	reader := bufio.NewReader(os.Stdin)
-	opt, _ := getInput("\nChoose an Account Type (0 - default, 1 - VIP): ", reader)
+	opt, _ := getInput("\nChoose an Account Type (0 - default, 1 - VIP, 99 - Cancel): ", reader)
+	var acc accountHolder
 
 	switch opt {
 	case "0":
@@ -41,78 +32,163 @@ func accountOptions(acc *accountHolder) {
 		a, err := strconv.ParseFloat(amt, 64)
 		if err != nil {
 			fmt.Println("Deposit amount must be a number!")
-			accountOptions(acc)
+			createAccountHolder(user)
 		} else if a < 10000.00 {
 			fmt.Println("You must deposit at least NGN 10,000")
-			accountOptions(acc)
+			createAccountHolder(user)
 		} else {
+			userName, _ := getInput("Enter Account Name:", reader)
+			acc = newAccount(userName)
 			acc.balance += a
-			fmt.Println("\n Account Created Successfully!")
+			fmt.Println("\nAccount Created Successfully!")
 			fmt.Println(acc.format())
 			acc.save()
 		}
 
 	case "1":
 		fmt.Println("VIP Account selected")
-		pin, _ := getInput("\nEnter your PIN to validate your VIP account: ", reader)
-
-		p, err := strconv.ParseUint(pin, 10, 64)
-		if err != nil {
-			fmt.Println("Enter a valid PIN")
-			accountOptions(acc)
-		} else if p != PIN {
-			fmt.Println("You have entered a wrong Pin. Please try again ")
-			accountOptions(acc)
-		} else if p == PIN {
-			fmt.Println("Your account validation was successful")
+		userName := accountValidation(user)
+		switch userName {
+		case "":
+			acc.accountName = ""
+		default:
+			acc.accountName = userName
 			acc.balance += 50000.00
 			acc.userClass = VIP
-			fmt.Println("\nAccount Created Successfully!")
+			fmt.Println("\n Account Created Successfully!")
 			fmt.Println(acc.format())
 			acc.save()
+
+		case "99":
+			fmt.Println("Thank you!!")
 		}
-
-	default:
-		fmt.Println("Select the correct option")
-		accountOptions(acc)
 	}
+	return acc
 }
 
+// function to validate VIP user before account creation
+func accountValidation(user map[int64]string) string {
+	reader := bufio.NewReader(os.Stdin)
+	pin, _ := getInput("\nEnter PIN: ", reader)
+	p, err := strconv.ParseInt(pin, 10, 64)
+	p1, found := user[p]
+	vip := ""
+	if err != nil {
+		fmt.Println("PIN must be numeric!")
+		accountValidation(user)
+	} else {
+		switch found {
+		case true:
+			fmt.Println("User", p1, "found")
+			vip = p1
+		case false:
+			fmt.Println("PIN does not exist!")
+			bl := YesNo("\nTry Again?")
+			switch bl {
+			case true:
+				accountValidation(user)
+			case false:
+				createAccountHolder(user)
+			}
+		}
+	}
+	return vip
+}
+
+// function to deposit into account
 func deposit(acc *accountHolder) {
-	reader := bufio.NewReader(os.Stdin)
-	amt, _ := getInput("\nEnter Amount to Deposit: ", reader)
-	a, err := strconv.ParseFloat(amt, 64)
-	if err != nil {
-		fmt.Println("Deposit amount must be a number!")
-		deposit(acc)
-	} else {
-		acc.credit(a)
-		acc.save()
+	switch acc.accountName {
+	case "":
+		fmt.Println("\nYou cannot deposit, You Do not have an account Yet")
+	default:
+		reader := bufio.NewReader(os.Stdin)
+		bl := YesNo("Do You want to make a deposit? ")
+		switch bl {
+		case true:
+			amt, _ := getInput("\nEnter Amount to Deposit: ", reader)
+			a, err := strconv.ParseFloat(amt, 64)
+			if err != nil {
+				fmt.Println("Deposit amount must be a number!")
+				deposit(acc)
+			} else {
+				acc.credit(a)
+				acc.save()
+			}
+		case false:
+			fmt.Println("Thank You")
+		}
 	}
 }
 
+// function to withdraw from account
 func withdraw(acc *accountHolder) {
-	reader := bufio.NewReader(os.Stdin)
-	amt, _ := getInput("\nEnter Amount to Withdraw: ", reader)
-	a, err := strconv.ParseFloat(amt, 64)
-	if err != nil {
-		fmt.Println("Withdrawal amount must be a number!")
-		withdraw(acc)
-	} else if a > acc.balance {
-		fmt.Println("Your balance is not enough to withdraw! Balance: ", acc.balance)
-		withdraw(acc)
-	} else {
-		acc.debit(a)
-		acc.save()
+	switch acc.accountName {
+	case "":
+		fmt.Println("\nYou caanot make a withdrawal, You Do not have an account Yet")
+	default:
+		reader := bufio.NewReader(os.Stdin)
+		bl := YesNo("Do You want to make a Withdrawal? ")
+		switch bl {
+		case true:
+			amt, _ := getInput("\nEnter Amount to withdraw: ", reader)
+			a, err := strconv.ParseFloat(amt, 64)
+			if err != nil {
+				fmt.Println("Withdrawal amount must be a number!")
+				withdraw(acc)
+			} else if a > acc.balance {
+				fmt.Println("Your balance is not enough to withdraw! Balance: ", acc.balance)
+				withdraw(acc)
+			} else {
+				acc.debit(a)
+				acc.save()
+			}
+		case false:
+			fmt.Println("Thank You")
+		}
 	}
+}
+
+//function to get Yes or No input from console
+func YesNo(msg string) bool {
+	reader := bufio.NewReader(os.Stdin)
+	sel, _ := getInput(msg+"Y - Yes,  N - No  ", reader)
+	s := strings.ToUpper(sel)
+	bl := true
+
+	switch s {
+	case "Y":
+		bl = true
+	case "N":
+		bl = false
+	default:
+		fmt.Println("Select the right option!")
+	}
+	return bl
+
 }
 func main() {
-	myAccount := createAccountHolder()
-	accountOptions(&myAccount)
+	// Pre-defined VIP users with PIN and Names
+	PIN := map[int64]string{
+		159: "Tim Jones",
+		753: "Sam Smith",
+		258: "Ada Stone",
+		953: "Ama Young",
+		751: "Kofi Poku",
+	}
+
+	// Create new accounts, verify PIN before creating VIP user
+	myAccount := createAccountHolder(PIN)
+
+	//Print account details to console
 	fmt.Println(myAccount.format())
+
+	//withdraw from account
 	withdraw(&myAccount)
-	fmt.Println(myAccount.format())
+
+	//deposit into account
 	deposit(&myAccount)
+
+	//Print account details into console
 	fmt.Println(myAccount.format())
 
 }
